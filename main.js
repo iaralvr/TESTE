6877ab8de1ab65574b8fb54a3f24339f4a311e19
iaralvr <129889774+iaralvr@users.noreply.github.com>
2023-04-05 12:43:41 -0300

    change

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ document.getElementById("botao"). addEventListener("click",  shownome);
 
 document.getElementById("btn-soma").addEventListener("click", soma);
 
-document.getElementById("btn-divisão").addEventListener("click", dividir);
+document.getElementById ( "btn-dividir" ).addEventListener ( "click" , dividir );
 
-document.getElementById("btn-multiplicação").addEventListener("click", multiplicar);
+document.getElementById ( "btn-multiplicar" ).addEventListener ( "click" , multiplicar );
+
+document.getElementById ( "btn-subtrair" ).addEventListener ( "click" ,subtrair );
 
 function showcolor()
 {
@@ -25,15 +27,23 @@ function soma()
     let num2 = document.getElementById("num2").value;
     document.getElementById( "resultado1" ).innerHTML= parseFloat(num1) + parseFloat(num2);
 }
-function divisão()
+function dividir ()
 {
-    let num3 = document.getElementById("num3").value;
-    let num4 = document.getElementById("num4").value;
-    document.getElementById( "resultado2" ).innerHTML= parseFloat(num3) / parseFloat(num4);
+    let num1 = document.getElementById("num3").value;
+    let num2 = document.getElementById("num4").value;
+    document.getElementById("resultado2").innerHTML = parseFloat (num1 ) / parseFloat (num2);
 }
-function multiplicação()
+
+function multiplicar ()
 {
-    let num5 = document.getElementById("num5").value;
-    let num6 = document.getElementById("num6").value;
-    document.getElementById( "resultado3" ).innerHTML= parseFloat(num5) * parseFloat(num6);
+    let num1 = document.getElementById("num5").value;
+    let num2 = document.getElementById("num6").value;
+    document.getElementById("resultado3").innerHTML = parseFloat (num1 ) * parseFloat (num2);
+}
+
+function subtrair ()
+{
+    let num1 = document.getElementById("num7").value;
+    let num2 = document.getElementById("num8").value;
+    document.getElementById("resultado4").innerHTML = parseFloat (num1 ) - parseFloat (num2);
 }
