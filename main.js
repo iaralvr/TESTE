ef6973d0c7fffca5179a0ea79f7f3e935a42f465
iaralvr <129889774+iaralvr@users.noreply.github.com>
2023-04-05 12:30:37 -0300

    mudanças

--- main.js
+++ main.js
@@ -25,13 +25,13 @@ function soma()
 }
 function divisão()
 {
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
+    let num3 = document.getElementById("num3").value;
+    let num4 = document.getElementById("num4").value;
     document.getElementById( "resultado2" ).innerHTML= parseFloat(num1) / parseFloat(num2);
 }
 function multiplicação()
 {
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
+    let num5 = document.getElementById("num5").value;
+    let num6 = document.getElementById("num6").value;
     document.getElementById( "resultado3" ).innerHTML= parseFloat(num1) * parseFloat(num2);
 }
