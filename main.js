d03094ae13374c7173a84ac21ea1f88380d5f75f
iaralvr <129889774+iaralvr@users.noreply.github.com>
2023-04-05 12:07:00 -0300

    changes

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener("click",showcolor );
 
 document.getElementById("botao"). addEventListener("click",  shownome);
 
+document.getElementById("btn-soma").addEventListener("click", soma);
+
 function showcolor()
 {
     document.body.style.backgroundColor ="red";
@@ -12,4 +14,10 @@ function shownome()
     let usuario = document.getElementById("nome").value;
     document.getElementById( "mensagem" ).innerHTML= "Meu nome é: " + usuario;
     document.getElementById( "nome" ).value="";
+}
+function soma()
+{
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    document.getElementById( "resultado1" ).innerHTML= parseFloat(num1) + parseFloat(num2);
 }
